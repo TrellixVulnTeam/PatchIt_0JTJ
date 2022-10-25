35cd88f928af0c91d1e20d2535b3c110420b1fb8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-25 18:27:55 +0000

    Adding tarfile member sanitization to extractall()

--- Patch/install.py
+++ Patch/install.py
@@ -290,7 +290,26 @@ def installModernPatch(patch, name, version, author, mp, patchArchive):
 
         with tarfile.open(os.path.join(
                           patchLocation, patchArchive), "r") as tar_file:
-            tar_file.extractall(installPath)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar_file, installPath)
 
         # Display gameplay tip only if Patch was successfully installed
         logging.info("Display LEGO Racers gameplay tip")
